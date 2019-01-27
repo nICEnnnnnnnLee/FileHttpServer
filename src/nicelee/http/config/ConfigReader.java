1e3d66b87aa842d48dc8aba1ff644e7fe670e3ce
root <lijia0732@sina.com>
2019-01-27 16:47:52 +0800

    更改默认配置的设置方式,便于拓展

--- src/nicelee/http/config/ConfigReader.java
+++ src/nicelee/http/config/ConfigReader.java
@@ -3,54 +3,76 @@ package nicelee.http.config;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 //import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import nicelee.http.MainApplication;
+
 public class ConfigReader {
 	final static Pattern patternConfig = Pattern.compile("^[ ]*([0-9|a-z|A-Z|.|_]+)[ ]*=[ ]*([^ ]+.*$)");
-	
-	
+
 	/**
-	 *  返回key 所对应的value; 如无设置,返回默认值
+	 * 返回key 所对应的value; 如无设置,返回0
+	 * 
 	 * @param property
 	 * @return
 	 */
 	public static int getInt(String property) {
 		try {
 			return Integer.parseInt(System.getProperty(property));
-		}catch (Exception e) {
-			if("nicelee.server.port".equals(property)) {
-				return 6667;
-			}else if("nicelee.server.fixedPoolSize".equals(property)) {
-				return 30;
-			}else{
-				return 0;
-			}
+		} catch (Exception e) {
+			return 0;
 		}
 	}
-	
+
 	/**
-	 *  返回key 所对应的value; 如无默认设置,返回null
+	 * 返回key 所对应的value; 如无设置,返回0
+	 * 
+	 * @param property
+	 * @return
+	 */
+	public static long getLong(String property) {
+		try {
+			return Long.parseLong(System.getProperty(property));
+		} catch (Exception e) {
+			return 0;
+		}
+	}
+
+	/**
+	 * 返回key 所对应的value; 如无默认设置,返回null
+	 * 
 	 * @param property
 	 * @return
 	 */
 	public static String getString(String property) {
-		String value = System.getProperty(property);
-		if( value == null) {
-			if("nicelee.server.port".equals(property)) {
-				return "6667";
-			}else if("nicelee.server.fixedPoolSize".equals(property)) {
-				return "30";
-			}else if("nicelee.server.source".equals(property)) {
-				return ".";
-			}			
-		}
-		return value;
+		return System.getProperty(property);
 	}
-	
+
 	public static void initConfigs() {
+		// 先初始化默认值
 		BufferedReader buReader = null;
+		try {
+			InputStream in = MainApplication.class.getResourceAsStream("/resources/app.config");
+			buReader = new BufferedReader(new InputStreamReader(in));
+			String config;
+			while ((config = buReader.readLine()) != null) {
+				Matcher matcher = patternConfig.matcher(config);
+				if (matcher.find()) {
+					System.setProperty(matcher.group(1), matcher.group(2).trim());
+					//System.out.printf("  key-->value:  %s --> %s\r\n", matcher.group(1), matcher.group(2));
+				}
+			}
+			buReader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+		
+		//从配置文件读取
+		buReader = null;
 		System.out.println("----Config init begin...----");
 		try {
 			buReader = new BufferedReader(new FileReader("app.config"));
@@ -59,11 +81,11 @@ public class ConfigReader {
 				Matcher matcher = patternConfig.matcher(config);
 				if (matcher.find()) {
 					System.setProperty(matcher.group(1), matcher.group(2).trim());
-					System.out.printf("  key-->value:  %s --> %s\r\n",matcher.group(1),matcher.group(2));
+					System.out.printf("  key-->value:  %s --> %s\r\n", matcher.group(1), matcher.group(2));
 				}
 			}
 		} catch (IOException e) {
-			//e.printStackTrace();
+			// e.printStackTrace();
 		} finally {
 			try {
 				buReader.close();
@@ -72,28 +94,4 @@ public class ConfigReader {
 		}
 		System.out.println("----Config ini end...----");
 	}
-/* 配置直接放入System.properties
-	public static HashMap<String, String> configs = new HashMap<>();
-	public static HashMap<String, String> initConfigs() {
-		BufferedReader buReader = null;
-		try {
-			buReader = new BufferedReader(new FileReader("config.application"));
-			String config;
-			while ((config = buReader.readLine()) != null) {
-				Matcher matcher = patternConfig.matcher(config);
-				if (matcher.find()) {
-					configs.put(matcher.group(1), matcher.group(2).trim());
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				buReader.close();
-			} catch (Exception e) {
-			}
-		}
-		return configs;
-	}
-*/
 }
